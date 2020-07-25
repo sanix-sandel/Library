3ff9a84080338946745b8c3add4a35f1a5f12f0d
sanix sandel <sanicksikani@gmail.com>
2020-07-26 00:01:20 +0300

    equality with hibbernate

--- src/main/java/sanix/springframework/spring5webapp/model/Book.java
+++ src/main/java/sanix/springframework/spring5webapp/model/Book.java
@@ -2,6 +2,7 @@ package sanix.springframework.spring5webapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -9,6 +10,7 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
     private String publisher;
@@ -51,4 +53,28 @@ public class Book {
     public Set<Author> getAuthors(){return authors;}
 
     public void setAuthors(Set<Author>authors){this.authors=authors;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id!=null ? id.hashCode():0;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
 }
