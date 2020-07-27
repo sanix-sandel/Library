a4bdbbff5f594563b02ba2673bcf8a7d978032c5
sanix sandel <sanicksikani@gmail.com>
2020-07-27 10:33:07 +0300

    display list of authors

--- src/main/java/sanix/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/sanix/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -3,6 +3,6 @@ package sanix.springframework.spring5webapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import sanix.springframework.spring5webapp.model.Author;
 
-public interface AuthorRepository extends CrudRepository<Author, Long> {
+public interface AuthorRepository extends CrudRepository <Author, Long> {
 
 }
